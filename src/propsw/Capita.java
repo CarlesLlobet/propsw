ec8da0101bf11dd9521d61cf2b4e2112f143320a
Goalie7 <marcos13594@gmail.com>
2014-11-03 23:01:40 +0100

    Afegides funcions sense especificar

---
+++ src/propsw/Capita.java
@@ -0,0 +1,71 @@
+/*
+ * Autor: Marcos Pérez
+ * Grup 44 - 9.1
+ */
+
+package propsw;
+
+public class Capita {
+
+	private int id;
+	private Galaxia gal;
+	private ArrayList<Rebels> ContReb;
+	
+	public Capita(int id){
+		this.id = id;
+	}
+	public int get_id(){
+		return this.id;
+	}
+	
+	//GalaxiaCtrl
+	public void importar_galaxia(){
+		
+	}
+	
+	public void exportar_galaxia(){
+		
+	}
+	
+	public void esborrar_galaxia(){
+		
+	}
+	
+	public void nova_galaxia(){
+		
+	}
+	
+	//RebelCtrl
+	public void afegir_rebel(){
+		
+	}
+	
+	public void esborrar_rebel(){
+		
+	}
+	
+	public void llistar_rebels(){
+		
+	}
+	
+	public void importar_rebels(){
+		
+	}
+	
+	public void exportar_rebels(){
+		
+	}
+	
+	public void inici_exode(){
+		
+	}
+	
+	public void afegir_capita(){
+		
+	}
+	
+	public void restaurar_sistema(){
+		
+	}
+	
+}
