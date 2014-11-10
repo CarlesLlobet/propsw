734cd8a3f156f398a3e5c31f9dbbb999bff69749
MarcRonquillo <marc.ronq@gmail.com>
2014-11-10 23:08:20 +0100

    Classe Base només amb constructors

--- src/propsw/Base.java
+++ src/propsw/Base.java
@@ -8,20 +8,20 @@ package propsw;
 public class Base {
 	
 	//Paràmetres privats
-	private Integer id;
+	private int id;
 	private String nom;
 	
 	//Constructors
 	public Base(){
-		id=new Integer(-1);
-		nom=new String();
+		id=-1;
+		nom=null;
 	}
 	
 	public Base(int id,String nom){
-		this.id=new Integer(id);
+		this.id=id;
 		this.nom=nom;
 	}
-	
+	/*
 	//Getters
 	public Integer getBaseId(){
 		return this.id;
@@ -29,15 +29,13 @@ public class Base {
 	
 	//Setters
 	public boolean setBaseId(int nouId){
-		/*El comprobar si l'id ja existeix s'ha de fer des de la classe Galaxia, 
-		 * que tindrà el hashmap amb totes les bases
-		 */
+		 
 		this.id=nouId;
 		return true;
 	}
 	
 	public void setBaseNom(String nouNom){
-		this.nom=nouNom;
-	}
+		nom=nouNom;
+	}*/
 
 }
