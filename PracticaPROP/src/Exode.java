c6b8bc225b385f308d2af9662450359569199525
AlbertTost <albert.tost@gmail.com>
2014-11-12 16:45:45 +0100

    Clase exode

---
+++ PracticaPROP/src/Exode.java
@@ -0,0 +1,43 @@
+// autor: Albert Tost
+
+import java.lang.String;
+
+public class Exode {
+	public int idBaseInici;
+	public ArrayList<Rebel> rebels;
+	public int camins;
+	public int destins;
+	public Graf grafResidual;
+	public Graf grafInicial;
+	public int flow;
+	public double cost;
+	public boolean actualitzat;
+	public String idExode;
+	public Galaxia MISSING;
+	public FordFoulkerson MISSING;
+
+	public ArrayList<Integer> getDestins() {
+		return null;
+	}
+
+	public void execucioDijkstra() {
+	}
+
+	public void execucioDFS() {
+	}
+
+	public void execucioBFS() {
+	}
+
+	public boolean afegirRebel(String idRebel) {
+		return false;
+	}
+
+	public boolean borrarRebel(String idRebel) {
+		return false;
+	}
+
+	public ArrayList<Integer> getAmpolla() {
+		return null;
+	}
+}
