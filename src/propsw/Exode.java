c93aa701a972e827b8fb1a3ee57e5285cffc38de
Goalie7 <marcos13594@gmail.com>
2014-11-17 11:04:43 +0100

    exode

--- src/propsw/Exode.java
+++ src/propsw/Exode.java
@@ -1,5 +1,7 @@
-//Autor: Albert Tost
-
+/*
+ * @Author: Albert Tost
+ * Grup 44 - 9.1
+ */ 
 package propsw;
 
 import java.io.IOException;
@@ -24,6 +26,12 @@ public class Exode {
 	private FordFulkerson<Base> ff;
 	private HashMap<Integer,ArrayList<Integer[]>> collsAmpolla;
 	
+	
+	/**
+	 * Crea un Exode de la galaxia g.
+	 * @param g
+	 * @throws IOException
+	 */
 	public Exode(Galaxia g) throws IOException{
 		++cont;
 		this.idExode = cont.toString();
@@ -35,12 +43,19 @@ public class Exode {
 		
 	}
 	
-	//Retorna un HashMap amb key=idRebel i value = idBase (Destí assignat al Rebel)
+	
+	/**
+	 * Consulta els dest’ assignat a cada rebel.
+	 * @return HashMap amb key=idRebel i value = idBase (Dest’ assignat al Rebel)
+	 */
 	public HashMap<String,Integer> getDestinsRebels(){
 		return destins;
 	}
 	
-	//Retorna un hashMap amb key = idBase (cada un dels destíns) i value = numero de rebels que van a aquell destí 
+	/**
+	 * Consulta del numero de rebels que van a cada un dels destins.
+	 * @return Retorna hashMap amb key = idBase (cada un dels destins) i value = numero de rebels que van a aquell dest’.
+	 */
 	public HashMap<Integer,Integer> getDestinsResum() {
 		//Cada rebel té un destí assignat. Es fa una llista 
 		//dels diferents destins sense repeticions
@@ -61,35 +76,45 @@ public class Exode {
 		}
 		return toReturn;
 	}
-	//Execucio de l'algoritme Dijsktra
+	
+	/** 
+	 * Execuci— de l'algoritme Dijkstra
+	 * @throws IOException
+	 */
 	public void execucioDijkstra() throws IOException {
 		ff = new FFDijkstra<Base>(idBaseInici,2,galaxia);
 		execucio();
 		
 	}
 	
-	
-	//Retorna un HashMap on la key es el id del Rebel y el value 
-	//es un ArrayList amb les bases que marquen el camí en ordre 
-	//desde l'origen al destí
+	/**
+	 * Consulta els camins de cada rebel des de l'origen al seu dest’.
+	 * @return HashMap on key = id del Rebel i el value es un ArrayList amb les bases que marquen el cam’ en ordre.
+	 */
 	public HashMap<String, ArrayList<Integer>> getCamins() {
 		return camins;
 	}
 	
-	// Executa el FF-DFS i emplena la variable camins assignant un camí a cada rebel assignat a l'exode.
+	/**
+	 * Execuci— de l'algoritme DFS i emplena la variable camins assignant un cam’ a cada rebel assignat a l'éxode.
+	 * @throws IOException
+	 */
 	public void execucioDFS() throws IOException {
 		ff = new FordFulkerson<Base>();
 		execucio();
 	}
 	
-	
+	/**
+	 * Execuci— de l'algoritme FordFoulkerson
+	 * @throws IOException
+	 */
 	private void execucio() throws IOException{
 		cost = 0;
 		camins = new HashMap<String, ArrayList<Integer>>();				//Reinicialitzem el mapa de camins  perque farem una nova execució
 		collsAmpolla = new HashMap<Integer, ArrayList<Integer[]>>();	//Reinicialitzem els colls d'ampolla perque fem una nova execució de l'algoritme
 		// Hem de unir els destins amb el sumidero en el graf inicial
 		Base b = new Base(galaxia);
-		grafResidualPostProces =  galaxia.getCopiaGraf();									//TODO: es passa la referencia i no la copia!! Aixo fara que al acabar la execucuó el graf inicial sigui el graf final
+		grafResidualPostProces =  galaxia.getCopiaGraf();
 		HashMap<Integer,Integer> destinsResum = getDestinsResum();
 		ArrayList<Integer> dests = new ArrayList<Integer>(destinsResum.keySet());
 		int sizeDests = dests.size();
@@ -106,9 +131,18 @@ public class Exode {
 		actualitzat = true;
 	}
 
-	//Donat un graf residual, recursivament, el va recorrent desde el node inicial fins al sumidero 
-	//en funció del fluxe assignat a cada aresta i al final del recorregut assigna 
-	//camins als rebels en funció del fluxe resultant i del destí que tenien assignat.
+	
+	/**
+	 * Donat un graf residual, recursivament, el va recorrent desde el node inicial fins al sumidero
+	   en funci— del fluxe assignat a cada aresta i al final del recorregut assigna 
+	   camins als rebels en funci— del fluxe resultant i del dest’ que tenien assignat.
+	 * @param g
+	 * @param idBase
+	 * @param camino
+	 * @param numRebels
+	 * @param destinsResum
+	 * @throws IOException
+	 */
 	private void generaCamins(Graf<Base> g, int idBase, ArrayList<Integer> camino, int numRebels, HashMap<Integer, Integer> destinsResum) throws IOException {
 		ArrayList<Integer> newCamino = new ArrayList<Integer>(camino);
 		newCamino.add(idBase);
@@ -119,15 +153,15 @@ public class Exode {
 			if(g.getFlujoAresta(g.getIDAresta(idBase,integer))>0)b=true;			//S'activa a true només si té com a minim un flux positiu en cualsevol aresta.
 		}
 		if(size<=0 || !b){
-			//Si no té arestes de sortida vol dir que hem arribat al sumidero. Assignem el camí a
+			//Si no tŽ arestes de sortida vol dir que hem arribat al sumidero. Assignem el camí a
 			//"n=numRebels" que encara no tinguin assignat cap camí
 			assignaRebels(newCamino,numRebels,g,destinsResum);
 			return;
 		}
 		int i = 0, fHelp, flujo = Integer.MAX_VALUE;
 		while (i < size && numRebels>0) {
-			//Donada una base uns rebels marxaràn cap a unes bases y uns altres cap a unes altres,
-			//En funció del flux que ha calculat el graf residual per cada aresta de sortida de la base.
+			//Donada una base uns rebels marxaran cap a unes bases y uns altres cap a unes altres,
+			//En funcio del flux que ha calculat el graf residual per cada aresta de sortida de la base.
 			fHelp = g.getFlujoAresta(g.getIDAresta(idBase, nodesOut.get(i)));			//No tenemos en cuenta las restroarestas
 			if(fHelp>0){
 				flujo = Math.min(numRebels, fHelp);
@@ -142,10 +176,15 @@ public class Exode {
 	}
 	
 	
-	
-	//Assigna el cami als rebels que tenen el destí igual que la penultima posició del array camino
-	//(la última es el sumidero). Només assigna camins a rebels que no tenen encara cap
-	//camí assignat....
+	/**
+	 * Assigna el cam’ als rebels que tenen el dest’ igual que la penœltima posici— del array camino
+	   (la œltima es el sumidero). NomŽs assigna camins a rebels que no tenen encara cap cam’ assignat.
+	 * @param camino
+	 * @param numRebels
+	 * @param g
+	 * @param destinsResum
+	 * @throws IOException
+	 */
 	private void assignaRebels(ArrayList<Integer> camino, int numRebels, Graf<Base> g, HashMap<Integer, Integer> destinsResum) throws IOException {
 		Iterator<String> it = destins.keySet().iterator();
 		camino.remove(camino.size()-1);
@@ -181,13 +220,24 @@ public class Exode {
 		}
 	}
 
-	//Execucio de l'algoritme EdmondKarp
+	
+	/**
+	 * Execuci— de l'algoritme EdmondKarp
+	 * @throws IOException
+	 */
 	public void execucioBFS() throws IOException {
 		ff = new EdmondsKarp<Base>(idBaseInici, 2, galaxia);
 		execucio();
 	}
 	
-	//S'afegeix un rebel a l'exode només si aquest forma part de la tropa del capità
+	//S'afegeix un rebel a l'exode només si aquest forma part de la tropa del capitˆ
+	/**
+	 * Afegeix un rebel a l'exode nomŽs si aquest forma part de la tropa del capitˆ.
+	 * @param idRebel
+	 * @param idBaseDesti
+	 * @return Retorna false si no forma part dels rebels del capitˆ 
+	 		perque no s'haura afegit la inserci—.
+	 */
 	public boolean afegirRebel(String idRebel, Integer idBaseDesti) {
 		Rebel r = galaxia.getCapita().getRebels().get(idRebel);
 		if(r!=null){
@@ -200,14 +250,22 @@ public class Exode {
 		}
 	}
 	
-	//Modifica la base destí del rebel. Es marca com a desactualitzat el rebel
+	
+	/**
+	 * Modifica la base dest’ del rebel. Es marca com a desactualitzat el rebel.
+	 * @param idRebel
+	 * @param idBaseDesti
+	 */
 	public void modificarRebel(String idRebel,Integer idBaseDesti) {
 		destins.put(idRebel, idBaseDesti);
 		actualitzat = false;		
 	}
 
-	//Borra un rebel del exode i marca com a desactualitzat el exode. Retorna true si existia el rebel 
-	//assignat al exode o false en cas contrari.
+	/**
+	 * Borra un rebel del exode i marca com a desactualitzat el exode.
+	 * @param idRebel
+	 * @return Retorna true si existia el rebel assignat al exode o false en cas contrari.
+	 */
 	public boolean borrarRebel(String idRebel) {
 		Integer r = destins.remove(idRebel);
 		actualitzat = false;
@@ -216,18 +274,36 @@ public class Exode {
 
 	// Getters i setters
 	
+	/**
+	 * Consulta del FordFoulkerson.
+	 * @return Retorna l'objecte de la classe FordFoulkerson.
+	 */
 	public FordFulkerson<Base> getFf() {
 		return ff;
 	}
-
+	
+	/**
+	 * Consulta del identificador de l'éxode.
+	 * @return String identificador de l'éxode.
+	 */
 	public String getIdExode(){
 		return idExode;
 	}
 	
+	/**
+	 * Consulta de l'identificador de la base d'inici.
+	 * @return Integer identificador base inici.
+	 */
 	public Integer getIdBaseInici() {
 		return idBaseInici;	
 	}
-	//Retorna false si la base no existeix dins de la galaxia.
+	
+	/**
+	 * Modifica l'identificador de la base d'inici.
+	 * @param idBaseInici
+	 * @return Retorna false si la base no existeix dins de la galˆxia.
+	 * @throws IOException
+	 */
 	public boolean setIdBaseInici(Integer idBaseInici) throws IOException {
 		Base r = galaxia.getNode(idBaseInici);
 		if(r!=null){
@@ -237,40 +313,63 @@ public class Exode {
 			return false;
 		}
 	}
-
+	
+	/**
+	 * Consulta dels rebels de l'éxode.
+	 * @return ArrayList<String> dels destins dels rebels.
+	 */
 	public ArrayList<String> getRebels() {
 		return new ArrayList<String>(destins.keySet());
 	}
 
-	
-
+	/**
+	 * Consulta el cost
+	 * @return Double del cost.
+	 */
 	public Double getCost() {
 		return cost;
 	}
 
-
-	
+	/**
+	 * Consulta del flow
+	 * @return Integer del flow.
+	 */
 	public Integer getFlow() {
 		return flow;
 	}
 	
-
-	
+	/**
+	 * Consulta si l'éxode estˆ actualitzat o no.
+	 * @return Booleˆ amb true si estˆ actualitzat i false si no n'estˆ.
+	 */
 	public boolean isActualitzat() {
 		return actualitzat;
 	}
 
+	/**
+	 * Consulta els colls d'ampolla.
+	 * @return HashMap en el qual es mostren els colls d'ampolla.
+	 */
 	public HashMap<Integer, ArrayList<Integer[]>> getCollsAmpolla() {
 		return collsAmpolla;
 	}
 
-
+	/**
+	 * Consulta el cam’ d'un determinat rebel amb identificador idRebel.
+	 * @param idRebel
+	 * @return ArrayList<Integer> que indica les bases que passa el rebel amb idRebel.
+	 */
 	public ArrayList<Integer> getCamiRebel(String idRebel) {
 		ArrayList<Integer> cami = camins.get(idRebel);
 		return cami;
 	}
 
 	//Retorna els identificadors de les arestes que son colls d'ampolla
+	/**
+	 * Consulta els identificadors de les arestes colls d'ampolla.
+	 * @return ArrayList<Integer> amb els identificadors de les arestes que s—n colls d'ampolla.
+	 * @throws IOException
+	 */
 	public ArrayList<Integer> getAmpolla() throws IOException {
 		HashMap<Integer,Object> toReturn = new HashMap<Integer,Object>();
 		Iterator<Integer> it = collsAmpolla.keySet().iterator();
@@ -283,7 +382,12 @@ public class Exode {
 		return new ArrayList<Integer>(toReturn.keySet());
 	}
 
-	//Retorna els identificadors de les bases (Integer[0]=from , Integer[1]=to) de les arestes que son colls d'ampolla
+	/**
+	 * Consulta identificadors de les bases que s—n coll
+	 * @return Retorna els identificadors de les bases (Integer[0]=from , Integer[1]=to)
+	           de les arestes que s—n colls d'ampolla.
+	 * @throws IOException
+	 */
 	public ArrayList<Integer[]> getAmpollaIdBases() throws IOException {
 		HashMap<String,Integer[]> toReturn = new HashMap<String,Integer[]>();
 		Iterator<Integer> it = collsAmpolla.keySet().iterator();
@@ -296,22 +400,28 @@ public class Exode {
 		return new ArrayList<Integer[]>(toReturn.values());
 	}
 
-
+	/**
+	 * Consulta el graf inicial.
+	 * @return Graf<Base> inicial abans de l'execuci— de l'algoritme.
+	 */
 	public Graf<Base> getGrafInicial() {
 		return grafInicial;
 	}
 
-
+	/**
+	 * Consulta la galˆxia.
+	 * @return objecte de tipus Galaxia.
+	 */
 	public Galaxia getGalaxia() {
 		return galaxia;
 	}
-
+	
+	/**
+	 * Consulta el graf residual.
+	 * @return Graf<Base> residual desprŽs d'executar l'algoritme.
+	 */
 	public Graf<Base> getGrafResidual() {
 		return grafResidualPreProces;
 	}
-	
-	
-	
-	
 	
 }
