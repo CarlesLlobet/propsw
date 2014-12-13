262d5c7533af4bd260994d06219ea053476b7ba8
arnaubennassar <arnau.bennasar@est.fib.upc.edu>
2014-12-14 00:38:37 +0100

    Update LlegirEscriureNatural.java

--- propsw/LlegirEscriureNatural.java
+++ propsw/LlegirEscriureNatural.java
@@ -8,44 +8,40 @@ import java.util.Scanner;
 public class LlegirEscriureNatural {
 	CapitaControlador caco;
 	GalaxiaControlador gaco;
-	String idCapi;
+	String idCapi = "shit";
 	LlegirEscriureNatural(CapitaControlador cc){
 		caco = cc;
 		gaco = new GalaxiaControlador(cc);
 	}
 	
-	public void llegirCapita(String ruta) throws IOException{		
+	public String llegirCapita(String ruta) throws IOException{		
 		File fitxer = new File(ruta);     
 	    if ( fitxer.exists() ){                                             
 	        Scanner inFile = new Scanner( fitxer );
 	        while ( inFile.hasNext() ){
 	           String line = inFile.nextLine();   // read the next line
-	           String delims = "[,:]";
-	           String[] tokens = line.split(delims);
-	           if (tokens[0] == "Capita"){      //"Capita:nom,password"
-	        	   //tokens[1] == nom
-	        	   //tokens[2] == pass
+	           String delims = "[:,]";
+	           String[] tokens = line.split(delims);	           
+	           if (tokens[0].equals("Capita")){      //"Capita:nom,password"
 	        	   Capita c = new Capita(tokens[1]);
 	        	   c.setPassword(tokens[2]);
 	        	   idCapi = c.getId();
 	        	   caco.afegirCapita(c);
 	           }
-	           else if (tokens[0] == "Nombre rebels"){ //"Nombre rebels:n"
-	        	   //tokens[1] == nombre rebels   
+	           else if (tokens[0].equals("Nombre rebels")){ //"Nombre rebels:n"
 	        	   int number = Integer.parseInt( tokens[1] );
 	        	   for (int i =0; i < number; ++i){
 	        		   caco.crearRebel(i+"", idCapi);
 	        	   }
 	        	   
 	           }
-	           else if (tokens[0] == "Nombre bases"){ //"Nombre bases:n"
-	        	   //tokens[1] == nombre bases  
+	           else if (tokens[0].equals("Nombre bases")){ //"Nombre bases:n"
 	        	   int number = Integer.parseInt( tokens[1] );
 	        	   for (int i =0; i < number; ++i){
 	        		   gaco.addBase(i+"", idCapi);
 	        	   }
 	           }
-	           else if (tokens[0] == "Conexio"){ //"Conexio:a,b,c,q.q"
+	           else if (tokens[0].equals("Conexio")){ //"Conexio:a,b,c,q.q"
 	        	   int from = Integer.parseInt( tokens[1] );
 	        	   int to = Integer.parseInt( tokens[2] );
 	        	   int capacitat = Integer.parseInt( tokens[3] );
@@ -54,7 +50,9 @@ public class LlegirEscriureNatural {
 	           }
 	        }
 	        inFile.close();
+	        return idCapi;
 	    }
+	    return "error";
 	}
 	
 	public void llegirGalaxia(String ruta, String idCapita) throws IOException{
@@ -63,18 +61,18 @@ public class LlegirEscriureNatural {
 	    if ( fitxer.exists() ){                                             
 	        Scanner inFile = new Scanner( fitxer );
 	        gaco.resetGalaxia(idCapi);
-	        while ( inFile.hasNext() ){
+	        while ( inFile.hasNext() ){	        	
 	           String line = inFile.nextLine();   // read the next line
 	           String delims = "[,:]";
 	           String[] tokens = line.split(delims);
-	          if (tokens[0] == "Nombre bases"){ //"Nombre bases:n"
+	          if (tokens[0].equals("Nombre bases")){ //"Nombre bases:n"
 	        	   //tokens[1] == nombre bases  
 	        	   int number = Integer.parseInt( tokens[1] );
 	        	   for (int i =0; i < number; ++i){
 	        		   gaco.addBase(i+"", idCapi);
 	        	   }
 	           }
-	           else if (tokens[0] == "Conexio"){ //"Conexio:a,b,c,q.q"
+	           else if (tokens[0].equals("Conexio")){ //"Conexio:a,b,c,q.q"
 	        	   int from = Integer.parseInt( tokens[1] );
 	        	   int to = Integer.parseInt( tokens[2] );
 	        	   int capacitat = Integer.parseInt( tokens[3] );
@@ -96,9 +94,9 @@ public class LlegirEscriureNatural {
         int nBases = c.getGalaxia().getBaseArray().size();
         ps.println("Nombre bases:"+Integer.toString( nBases ));
         for(int i = 0; i < nBases; ++i){
-            for(int j = 0; j < nBases; ++i){
-            	if (gaco.existsAdjacency(i, j)){
-            		ps.println("Conexio:"+ Integer.toString(i) +","+ Integer.toString(j) +","+ Integer.toString(gaco.getCapacitatAdjacency(i, j)) +","+ Double.toString(gaco.getCostAdjacency(i, j)));
+            for(int j = 0; j < nBases; ++j){
+            	if (gaco.existsAdjacency(i, j, idCapi)){
+            		ps.println("Conexio:"+ Integer.toString(i) +","+ Integer.toString(j) +","+ Integer.toString(gaco.getCapacitatAdjacency(i, j, idCapi)) +","+ Double.toString(gaco.getCostAdjacency(i, j, idCapi)));
             	}
             }
         }
@@ -107,15 +105,16 @@ public class LlegirEscriureNatural {
 	
 	//SI EL FITXER JA EXISTEIX SOBRESCRIURA!
 	public void escriureGalaxia(String outFileName, String idCapita) throws IOException{
+		 
 		idCapi = idCapita;
 		Capita c = caco.getCapita(idCapi);
         java.io.PrintStream ps = new java.io.PrintStream( outFileName+".txt" );
         int nBases = c.getGalaxia().getBaseArray().size();
         ps.println("Nombre bases:"+Integer.toString( nBases ));
         for(int i = 0; i < nBases; ++i){
-            for(int j = 0; j < nBases; ++i){
-            	if (gaco.existsAdjacency(i, j)){
-            		ps.println("Conexio:"+ Integer.toString(i) +","+ Integer.toString(j) +","+ Integer.toString(gaco.getCapacitatAdjacency(i, j)) +","+ Double.toString(gaco.getCostAdjacency(i, j)));
+            for(int j = 0; j < nBases; ++j){
+            	if (gaco.existsAdjacency(i, j, idCapi)){
+            		ps.println("Conexio:"+ Integer.toString(i) +","+ Integer.toString(j) +","+ Integer.toString(gaco.getCapacitatAdjacency(i, j, idCapi)) +","+ Double.toString(gaco.getCostAdjacency(i, j, idCapi)));
             	}
             }
         }
