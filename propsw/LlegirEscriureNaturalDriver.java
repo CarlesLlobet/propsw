b44d4446e30dedb0dc9c8256c18349a6ad25cf38
arnaubennassar <arnau.bennasar@est.fib.upc.edu>
2014-12-14 00:45:37 +0100

    Update LlegirEscriureNaturalDriver.java

--- propsw/LlegirEscriureNaturalDriver.java
+++ propsw/LlegirEscriureNaturalDriver.java
@@ -6,9 +6,9 @@ public class LlegirEscriureNaturalDriver {
 	public static void main(String[] args) throws IOException {
 		CapitaControlador cc = new CapitaControlador(); 
 		LlegirEscriureNatural len = new LlegirEscriureNatural(cc);
-		String ic = len.llegirCapita("prova.txt");
-		len.escriureCapita("provaOut", ic);
-		len.llegirGalaxia("provag.txt", ic);
-		len.escriureGalaxia("provaOutg", ic);
+		String ic = len.llegirCapita("provaLecturaCapi.txt");
+		len.escriureCapita("provaOutCapi", ic);
+		len.llegirGalaxia("provaLecturaGal.txt", ic);
+		len.escriureGalaxia("provaOutGal", ic);
 	}
 }
