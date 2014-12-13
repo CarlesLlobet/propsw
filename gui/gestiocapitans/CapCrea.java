68edd8d717abb823bd662491e3556471d6caf02c
albert.tost <albert.tost@gmail.com>
2014-12-13 19:50:50 +0100

    gestio capitaaaa

---
+++ gui/gestiocapitans/CapCrea.java
@@ -0,0 +1,16 @@
+package gui.gestiocapitans;
+
+import gui.Principal;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+
+public class GalDraw extends JPanel{
+	public GalDraw() {
+		setBackground(Color.PINK);
+		System.out.println("CREA CAPITA");
+	}
+	
+}
